3100e18ddddee04c697dbe85c416845bf4c5fb3c
Yusuf Talha Sen <yussen@student.42istanbul.com.tr>
2025-07-07 17:18:31 +0300

    final

--- calculator.c
+++ calculator.c
@@ -6,7 +6,7 @@
 /*   By: yussen <yussen@student.42istanbul.com.t    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/28 09:31:42 by yussen            #+#    #+#             */
-/*   Updated: 2025/06/28 11:18:54 by yussen           ###   ########.fr       */
+/*   Updated: 2025/07/07 17:17:18 by yussen           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,32 +17,57 @@ int	ft_calculator(char sign , int c, ...)
 	va_list	args;
 	int	i = 0;
 	int	a = 0;
-	int	b = 1;
+	int x = 1;
+	int d = 0;
+	int division = 0;
 	int	flag = 0;
+	va_list *temp;
 	
+	temp = &args;
 	va_start(args, c);
+	if (sign == '/')
+	{
+		division = va_arg(*temp, int);
+		i++;
+	}
+	if (sign == '-')
+	{
+		d = va_arg(*temp, int);
+		i++;
+	}
 	while (i < c)
 	{
 		if (sign == '+')
-			a += va_arg(args, int);
+		{
+			a += va_arg(*temp, int);
+			i++;
+		}
 		if (sign == '-')
-			a -= va_arg(args, int);
+		{
+			d -= va_arg(*temp, int);
+			flag = 2;
+			i++;
+		}
 		if (sign == '*')
 		{
-			b *= va_arg(args, int);
-			flag++;
+			x *= va_arg(*temp, int);
+			i++;
+			flag = 3;
 		}
 		if (sign == '/')
 		{
-			a = va_arg(args, int);
-			a /= va_arg(args, int);
+			division /= va_arg(*temp, int);
 			i++;
+			flag = 4;
 		}
-		i++;
 	}
 	va_end(args);
-	if (flag)
-		return (b);
+	if (flag == 2)
+		return (d);
+	else if(flag == 3)
+		return (x);
+	else if (flag == 4)
+		return (division);
 	else
 		return(a);
 }
